57911f31e5c8dbc9dae4dde64b3477f3455d78d4
A VINAY KALYAN REDDY <114601857+vinaykaly@users.noreply.github.com>
2023-08-10 00:33:09 +0530

    Update leetcode_118.py

--- leetcode_118.py
+++ leetcode_118.py
@@ -4,21 +4,23 @@ Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]"""
 res = [[1],[1,1]]
 
 if numRows == 1 or numRows == 2:
-  print(res[:numRows])
+    print (res[:numRows])
 
 else:
-    z = [1,1]   # to sum the last element of the res or holder
-
+    
+    z = [1,1]
     while True:
-       x = [1,1] #insert the sum value
-       y = 1
+        y = 1
+        x = [1,1]
+        
+        for i in list(range(len(res[len(res)-1])-1)):
+            x.insert(y,z[i]+z[i+1])
+            y = y+1
 
-       for i in range(len(res[len(res)-1]-1)):
-          x.insert(y,z[i]+z[i+1])
-          y = y+1
-       res.append(x); z = x
-       if len(res) == numRows:
-          print(res);break
-
+        res.append(x)
+        z = x
+        
+        if len(res) == numRows:
+            print (res)
   
        
