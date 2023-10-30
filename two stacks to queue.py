4cef959d2e087b0c0512d1f0a6a229b94d96cc33
A VINAY KALYAN REDDY <114601857+vinaykaly@users.noreply.github.com>
2023-10-30 12:25:47 +0530

    Create two stacks to queue.py
     implement a queue using two stacks.

---
+++ two stacks to queue.py
@@ -0,0 +1,22 @@
+ #implement a queue using two stacks.
+
+class queue2stacks:
+
+  def __init__(self):
+    self.stack1 = []
+    self.stack2 = []
+
+  def enqueue(self,value):
+    self.stack1.append(value)
+
+  def dequeue(self):
+    if stack1:
+     if not stack2 :
+       while self.stack1:
+
+         self.stack2.append(self.stack1.pop())
+     return self.stack.pop()
+
+x = queue2stacks()
+
+        
